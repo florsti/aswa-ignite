fe5bd4b2b36160ce47cfdca80974fbacfa4e7973
florsti <80473574+florsti@users.noreply.github.com>
2021-04-09 12:10:38 +0200

    Update index.js

--- api/tasks/index.js
+++ api/tasks/index.js
@@ -3,10 +3,10 @@ module.exports = async function (context, req) {
     // setup our default content type (we always return JSON)
     context.log('returning tasks');
     const tasks = [
-     {name: 'Azure'},
-     {name: 'Sammy'},
-     {name: 'Azury'},
-     {name: 'Rolfs'}
+     {name: 'Azure', desc: 'eerste'},
+     {name: 'Sammy', desc: 'tweede'},
+     {name: 'Azury', desc: 'derde'},
+     {name: 'Rolfs', desc: 'vierde'}
     ]
     context.res = {
         body: {
